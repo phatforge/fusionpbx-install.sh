46fa0fff7ace1f4d25577c1554cb4a4365137ff2
Pritesh <pritesh@phatforge.com>
2018-02-06 16:22:12 +0000

    Create ubuntu specific version of install
    Adjust the preinstall script to install from phatforge for development

---
+++ ubuntu/pre-install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#upgrade the packages
+apt-get update && apt-get upgrade -y
+
+#install packages
+apt-get install -y git lsb-release
+
+#get the install script
+cd /usr/src && git clone https://github.com/phatforge/fusionpbx-install.sh.git
+
+#change the working directory
+cd /usr/src/fusionpbx-install.sh/ubuntu
